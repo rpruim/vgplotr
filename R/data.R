479d18e68c250eaeed03e032dfdc6e9163d99556
Randall Pruim <rpruim@gmail.com>
2026-09-22 22:31:42 -0400

    Rename vg_data_url() to vg_example_url()

--- R/data.R
+++ R/data.R
@@ -1,13 +1,14 @@
-#' Build URL for fetching uwdata/mosaic data sets
+#' Build the URL for one of mosaic's own example data sets
 #'
-#' @param file file name
-#' @return The URL as a string
+#' @param file The example data set's file name, as listed in
+#'   [mosaic's own `data/` directory](https://github.com/uwdata/mosaic/tree/main/data).
+#' @return The URL as a string.
 #' @family spec functions
 #' @export
 #' @examples
-#' vg_data_url('atheletes.csv')
+#' vg_example_url('athletes.parquet')
 #'
-vg_data_url <- function(file) {
+vg_example_url <- function(file) {
   paste0(
     "https://raw.githubusercontent.com/uwdata/mosaic/refs/heads/main/data/",
     file
